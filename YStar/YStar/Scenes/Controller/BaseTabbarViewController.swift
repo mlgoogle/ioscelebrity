b90a6e12cde995a6d2111ff13eb053febdaad828
mu <1755731709@qq.com>
2017-07-06 14:27:05 +0800

    切换成多故事板

--- YStar/YStar/Scenes/Controller/BaseTabbarViewController.swift
+++ YStar/YStar/Scenes/Controller/BaseTabbarViewController.swift
@@ -12,7 +12,18 @@ class BaseTabbarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let storyboardNames = ["Benifity","Fans","Meet"]
+        let itemIconNames = ["home","tour","user"]
+        let titles = ["收益管理","粉丝管理","约见管理"]
+        for (index, name) in storyboardNames.enumerated() {
+            let storyboard = UIStoryboard.init(name: name, bundle: nil)
+            let controller = storyboard.instantiateInitialViewController()
+            controller?.tabBarItem.title = titles[index]
+            controller?.tabBarItem.image = UIImage.init(named: "\(itemIconNames[index])_item_normal")?.withRenderingMode(.alwaysOriginal)
+            controller?.tabBarItem.selectedImage = UIImage.init(named: "\(itemIconNames[index])_item_selected")?.withRenderingMode(.alwaysOriginal)
+            controller?.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(rgbHex: AppConst.ColorKey.main.rawValue)], for: .selected)
+            addChildViewController(controller!)
+        }
     }
-
+    
 }
