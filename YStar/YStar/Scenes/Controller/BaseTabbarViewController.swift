a80309931f265242e838c7c22defffc9143d4386
qc_51vip@163.com <you@example.com>
2017-08-04 11:18:24 +0800

    fix tabbarItemImage

--- YStar/YStar/Scenes/Controller/BaseTabbarViewController.swift
+++ YStar/YStar/Scenes/Controller/BaseTabbarViewController.swift
@@ -18,16 +18,23 @@ class BaseTabbarViewController: UITabBarController,NIMLoginManagerDelegate,NIMCo
         
         let storyboardNames = ["Benifity","Fans","Meet"]
         
-        let itemIconNames = ["\u{e65e}","\u{e65b}","\u{e609}"]
+        let itemIconNames = ["\u{e65e}","\u{e65b}"]
         let titles = ["收益管理","粉丝管理","约见管理"]
         
         for (index, name) in storyboardNames.enumerated() {
             let storyboard = UIStoryboard.init(name: name, bundle: nil)
             let controller = storyboard.instantiateInitialViewController()
+            if index == 2 {
+                controller?.tabBarItem.title = titles[2]
+                controller?.tabBarItem.image = UIImage(named: "meetManager_normal")
+                controller?.tabBarItem.selectedImage = UIImage(named: "meetManager_selected")
+                 controller?.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(rgbHex: AppConst.ColorKey.main.rawValue)], for: .selected)
+            } else {
             controller?.tabBarItem.title = titles[index]
             controller?.tabBarItem.image = UIImage.imageWith(itemIconNames[index], fontSize: CGSize.init(width: 22, height: 22), fontColor: UIColor.init(rgbHex: 0x999999)).withRenderingMode(.alwaysOriginal)
             controller?.tabBarItem.selectedImage = UIImage.imageWith(itemIconNames[index], fontSize: CGSize.init(width: 22, height: 22), fontColor: UIColor.init(rgbHex: AppConst.ColorKey.main.rawValue)).withRenderingMode(.alwaysOriginal)
             controller?.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(rgbHex: AppConst.ColorKey.main.rawValue)], for: .selected)
+            }
             addChildViewController(controller!)
         }
         
@@ -45,7 +52,7 @@ class BaseTabbarViewController: UITabBarController,NIMLoginManagerDelegate,NIMCo
         NIMSDK.shared().systemNotificationManager.add(self)
         
         self.sessionUnreadCount = NIMSDK.shared().conversationManager.allUnreadCount()
-        print("未读消息条数====\(NIMSDK.shared().conversationManager.allUnreadCount())")
+        print("未读消息条数====\(self.sessionUnreadCount)")
         // 刷新红点
         self.refreshSessionRedDot()
     }
